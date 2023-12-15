a325b13d04a05e11c3f882fa7937b1df8e6d3da8
Jeann Andrade <jeann.andrade@outlook.com>
2023-12-14 23:44:36 -0300

    Arrumando as pastas

---
+++ CSharpLanguage/C# 3.0/QueryComprehensions/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] names = { "Tom", "Dick", "Harry" };
+
+// Using Query comprehension syntax
+var filteredNames = from n in names where n.Length >= 4 select n;
+
+filteredNames.ToList().ForEach(x => Console.WriteLine(x));
+
